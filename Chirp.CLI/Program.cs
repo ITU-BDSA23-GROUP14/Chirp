dad1f954670d3b1124a26b3056dc9feda6993529
Markus <mfau@itu.dk>
2023-09-04 15:22:19 +0200

    Add input arguments and AddChirp method
    Co-authored-by:
    Ida <idavilstrup@gmail.com>
    Niklas <naaa@itu.dk>
    Dagrún Eir  <daas@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -5,37 +5,60 @@
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Security.Cryptography.X509Certificates;
 
-try
+
+if (args[0] == "read") PrintChirps();    
+else if (args[0] == "cheep" && args.Length == 2) AddChirp();
+
+void AddChirp()
 {
-    using var sr = new StreamReader("chirp_cli_db.csv");                                                   // Open the text file using a stream reader.
-    var text = sr.ReadToEnd();                                                                          // Read the stream as a string, and write the string to the console.
+    string path = @"chirp_cli_db.csv";
+    string username = Environment.UserName;
+    long currentTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    
+    using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine();
+            sw.Write($"{username},\"{args[1]}\",{currentTime}");
+        }
+}
 
-    string[] lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);       // Split at every newline  
-    lines = lines.Skip(1).ToArray();                                                                    // Skip the first line, which contains the category names (Author,Message,Timestamp)                                                               
-
-    // Taken from https://stackoverflow.com/a/34265869
-    Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-    TimeZoneInfo cestZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
-
-    foreach (string l in lines)
+void PrintChirps()
+{
+    try
     {
-        string[] infoArr = CSVParser.Split(l);
-        var info = (author: infoArr[0],
-                    message: infoArr[1].Replace("\"", ""),                                              // Remove quotation marks from the messages stored in the .csv file
-                    timestamp: infoArr[2]);
-        var timeUtc = DateTimeOffset.FromUnixTimeSeconds(long.Parse(info.timestamp)).DateTime;          // Parse the timestap as a DateTime object
-        DateTime timeCet = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cestZone);                           // Convert the timestamp from UTC to CEST
+        using var sr = new StreamReader("chirp_cli_db.csv");                                                   // Open the text file using a stream reader.
+        var text = sr.ReadToEnd();                                                                          // Read the stream as a string, and write the string to the console.
 
-        var formattedTimestamp = timeCet.ToString("MM/dd/yy HH':'mm':'ss");
+        string[] lines = text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);       // Split at every newline  
+        lines = lines.Skip(1).ToArray();                                                                    // Skip the first line, which contains the category names (Author,Message,Timestamp)                                                               
 
-        Console.WriteLine($"{info.author} @ {formattedTimestamp}: {info.message}");
+        // Taken from https://stackoverflow.com/a/34265869
+        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
+        TimeZoneInfo cestZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+
+        foreach (string l in lines)
+        {
+            string[] infoArr = CSVParser.Split(l);
+            var info = (author: infoArr[0],
+                        message: infoArr[1].Replace("\"", ""),                                              // Remove quotation marks from the messages stored in the .csv file
+                        timestamp: infoArr[2]);
+            var timeUtc = DateTimeOffset.FromUnixTimeSeconds(long.Parse(info.timestamp)).DateTime;          // Parse the timestap as a DateTime object
+            DateTime timeCet = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cestZone);                           // Convert the timestamp from UTC to CEST
+
+            var formattedTimestamp = timeCet.ToString("MM/dd/yy HH':'mm':'ss");
+
+            Console.WriteLine($"{info.author} @ {formattedTimestamp}: {info.message}");
+        }
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("The file could not be read:");
+        Console.WriteLine(e.Message);
     }
 }
-catch (IOException e)
-{
-    Console.WriteLine("The file could not be read:");
-    Console.WriteLine(e.Message);
-}
+
+
     
