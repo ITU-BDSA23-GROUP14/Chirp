6603e96c5fa78ab3ac76e009402c601537a61648
Markus <mfau@itu.dk>
2023-09-11 15:57:36 +0200

    Add missing files
    Co-authored-by: Jonas <jfho@itu.dk>
    Co-authored-by: Ida <idavilstrup@gmail.com>
    Co-authored-by: Niklas <naaa@itu.dk>
    Co-authored-by: Dagrún Eir  <daas@itu.dk>

--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -1,6 +1,6 @@
 namespace SimpleDB;
 
-interface IDatabaseRepository<T>
+public interface IDatabaseRepository<T>
 {
     public IEnumerable<T> Read(int? limit = null);
     public void Store(T record);
