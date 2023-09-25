e7c07c2a85e3e745c7b2a651f49ddd22e75ca46f
Niklas Andersen <naaa@itu.dk>
2023-09-25 14:58:55 +0200

    Setup manual first deploy
    Co-authored-by: Jonas <jfho@itu.dk>
    Co-authored-by: Ida <idavilstrup@gmail.com>

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -12,3 +12,12 @@ app.MapGet("/cheeps", () => db.Read());
 app.MapPost("/cheep", (Cheep cheep) => { db.Store(cheep); });
 
 app.Run();
+
+/*
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+*/
