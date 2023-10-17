0e03f8169159a842494cd23a65bc3d99cb86a58b
Markus <mfau@itu.dk>
2023-10-17 16:55:54 +0200

    Add methods to get author, create author and cheep
    Co-authored-by: Dagrún Eir  <daas@itu.dk>

--- src/Chirp.Infrastructure/CheepRepository.cs
+++ src/Chirp.Infrastructure/CheepRepository.cs
@@ -1,4 +1,3 @@
-using Microsoft.EntityFrameworkCore;
 using Chirp.Core;
 
 namespace Chirp.Infrastructure;
@@ -11,6 +10,63 @@ public class CheepRepository : ICheepRepository
     {
         _dbContext = dbContext;
         DbInitializer.SeedDatabase(_dbContext);
+    }
+
+    public void CreateAuthor(string name, string email)
+    {
+        if (_dbContext.Authors.Any(a => a.Email == email))
+        {
+            throw new InvalidOperationException($"The email address {email} is already in use.");
+        }
+
+        if (_dbContext.Authors.Any(a => a.Name == name))
+        {
+            throw new InvalidOperationException($"The name {name} is already taken.");
+        }
+
+        var author = new Author { Name = name, Email = email };
+
+        _dbContext.Authors.Add(author);
+        _dbContext.SaveChanges();
+    }
+
+    public void CreateCheep(string cheepText, string authorName, string authorEmail)
+    {
+        // Check if author exists. If not, create the author
+        var author = _dbContext.Authors.FirstOrDefault(author => author.Name == authorName);
+        if (author == null)
+        {
+            author = new Author { Name = authorName, Email = authorEmail };
+            _dbContext.Add(author);
+            _dbContext.SaveChanges();
+        }
+
+        var cheep = new Cheep { Author = author, Text = cheepText };
+
+        _dbContext.Add(cheep);
+        _dbContext.SaveChanges();
+    }
+
+    public AuthorDTO? GetAuthorByEmail(string email)
+    {
+        var author = _dbContext.Authors.FirstOrDefault(author => author.Email == email);
+        if (author == null)
+        {
+            return null;
+        }
+
+        return new AuthorDTO { Name = author.Name };
+    }
+
+    public AuthorDTO? GetAuthorByName(string name)
+    {
+        var author = _dbContext.Authors.FirstOrDefault(author => author.Name == name);
+        if (author == null)
+        {
+            return null;
+        }
+
+        return new AuthorDTO { Name = author.Name };
     }
 
     public List<CheepDTO> GetCheepDTOs(int page)
@@ -45,4 +101,6 @@ public class CheepRepository : ICheepRepository
                     TimeStamp = c.TimeStamp.ToString("dd/MM/yy HH:mm:ss")
                 }).Skip(32 * page).Take(32).ToList();
     }
+
+
 }
