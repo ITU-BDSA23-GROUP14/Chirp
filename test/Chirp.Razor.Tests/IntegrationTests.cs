221f2f7455a0aa51018308c78dff5679eade93fc
jfho <jfho@itu.dk>
2023-10-24 14:30:33 +0200

    Add Assert, Act and Arrange comments

--- test/Chirp.Razor.Tests/IntegrationTests.cs
+++ test/Chirp.Razor.Tests/IntegrationTests.cs
@@ -10,6 +10,7 @@ namespace RazorTest
 
         public IntegrationTests(WebApplicationFactory<Program> fixture)
         {
+            // Arrange
             _fixture = fixture;
             _client = _fixture.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = true, HandleCookies = true });
         }
@@ -17,12 +18,14 @@ namespace RazorTest
         [Fact]
         public async void CanSeePublicTimeline()
         {
+            // Act
             var response = await _client.GetAsync("/");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
 
-            var cheepText = "Hello, BDSA students!";  // This is the earliest registered cheep (ie. it shows up on the first page and is thus included in the "/" response)
+            var cheepText = "Hello, BDSA students!";
 
+            // Assert
             Assert.Contains(cheepText, content);
         }
 
@@ -31,24 +34,29 @@ namespace RazorTest
         [InlineData("Rasmus", "Hej, velkommen til kurset.")]
         public async void CanSeePrivateTimeline(string author, string expectedMessage)
         {
+            // Act
             var response = await _client.GetAsync($"/{author}");
             response.EnsureSuccessStatusCode();
             var content = await response.Content.ReadAsStringAsync();
 
+            // Assert
             Assert.Contains(expectedMessage, content);
         }
 
         [Fact]
         public async void Page5000Loads()
         {
+            // Act
             var response = await _client.GetAsync("/?page=5000");
 
+            // Assert
             response.EnsureSuccessStatusCode();
         }
 
         [Fact]
         public async void Page0And1AreTheSame()
         {
+            // Act
             var page0Response = await _client.GetAsync("/?page=0");
             var page1Response = await _client.GetAsync("/?page=1");
 
@@ -57,12 +65,14 @@ namespace RazorTest
             var content1 = await page0Response.Content.ReadAsStringAsync();
             var content2 = await page1Response.Content.ReadAsStringAsync();
 
+            // Assert
             Assert.Contains(content1, content2);
         }
 
         [Fact]
         public async void Page1And2AreDifferent()
         {
+            // Act
             var page1Response = await _client.GetAsync("/?page=1");
             var page2Response = await _client.GetAsync("/?page=2");
 
@@ -71,12 +81,14 @@ namespace RazorTest
             var content1 = await page1Response.Content.ReadAsStringAsync();
             var content2 = await page2Response.Content.ReadAsStringAsync();
 
+            // Assert
             Assert.DoesNotContain(content1, content2);
         }
 
         [Fact]
         public async void Page1Contains32Cheeps()
         {
+            // Act
             var pageResponse = await _client.GetAsync("/?page=1");
 
             pageResponse.EnsureSuccessStatusCode();
@@ -85,6 +97,7 @@ namespace RazorTest
             // Adapted from https://stackoverflow.com/questions/541954/how-to-count-occurrences-of-a-char-string-within-a-string
             int count = content.Split("<li>").Length - 1;
 
+            // Assert
             Assert.Equal(32, count);
         }
     }
