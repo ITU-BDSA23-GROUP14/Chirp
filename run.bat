f51850ee3088c19b533cd63585a8144c2c2e18b8
Niklas Andersen <naaa@itu.dk>
2023-11-06 15:20:56 +0100

    Change password

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-docker run -e "ACCEPT_EULA=Y" -e "33eca922-74a0-11ee-9e21-00155d9a126b" -p 1433:1433 --name sql-server -d mcr.microsoft.com/mssql/server:2022-latest
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=33eca922-74a0-11ee-9e21-00155d9a126b" -p 1433:1433 --name sql-server -d mcr.microsoft.com/mssql/server:2022-latest
 
 dotnet run --project src/Chirp.Web
