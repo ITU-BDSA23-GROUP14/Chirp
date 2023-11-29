e43e19504a45ad9e172553b4ddf19bdcee9c0f53
jfho <jfho@itu.dk>
2023-11-29 23:17:48 +0100

    Create E2E mock auth and E2E tests

--- test/Chirp.Tests/E2ETests.cs
+++ test/Chirp.Tests/E2ETests.cs
@@ -1,23 +1,56 @@
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
 
 namespace Chirp.Tests;
 
-[Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class E2ETests : PageTest
 {
-    [Test]
-    public async Task PlaywrightTest1()
+    private readonly E2EWebApplicationFactory<Program> _fixture;
+    private readonly string _serverAddress;
+
+    public E2ETests()
     {
-        // Assertions use the expect API.
-        await Expect(Page).ToHaveURLAsync("https://playwright.dev/");
+        _fixture = new E2EWebApplicationFactory<Program>();
+        _serverAddress = _fixture.ServerAddress;
     }
 
     [SetUp]
     public async Task SetUp()
     {
-        await Page.GotoAsync("https://playwright.dev");
+        await Page.GotoAsync(_serverAddress);
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await Page.GotoAsync(_serverAddress);
+        var firstCheep = Page.GetByRole(AriaRole.Listitem).First;
+        await firstCheep.GetByRole(AriaRole.Button).First.ClickAsync();
+    }
+
+    [Test]
+    public async Task FollowTurnsToUnfollow()
+    {
+        var firstCheep = Page.GetByRole(AriaRole.Listitem).First;
+        await firstCheep.GetByRole(AriaRole.Button, new() { Name = "Follow" }).First.ClickAsync();
+
+        await Expect(firstCheep
+            .GetByRole(AriaRole.Button, new() { Name = "Unfollow" })
+            .First)
+            .ToBeVisibleAsync();
+    }
+
+    [Test]
+    public async Task PrivateTimelineShowsFollowingCheeps()
+    {
+        var firstCheep = Page.GetByRole(AriaRole.Listitem).First;
+        await firstCheep.GetByRole(AriaRole.Button, new() { Name = "Follow" }).First.ClickAsync();
+        await Page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+
+        await Expect(Page.GetByText("Jacqualine Gilcoine").First).ToBeVisibleAsync();
+        await Expect(Page.GetByText("Starbuck now is what we hear the worst.").First).ToBeVisibleAsync();
     }
 }
